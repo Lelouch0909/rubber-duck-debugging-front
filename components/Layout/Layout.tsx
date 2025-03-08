958bfc155f3259ba22672625474f44e55c95edcc
Ekodev <ekoledev@gmail.com>
2025-03-08 04:36:02 +0000

    refactor(components/header): mise a zero des configurations
    Suppression des composants de base

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { Header } from "@/components/Headers/Header";
-import { Theme } from "@radix-ui/themes";
-import { ThemeProvider } from "next-themes";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,11 +6,6 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <ThemeProvider attribute="class">
-      <Theme appearance="inherit">
-        <Header />
-        <main className="flex-1">{children}</main>
-      </Theme>
-    </ThemeProvider>
+    <main className="flex-1">{children}</main>
   );
 };
